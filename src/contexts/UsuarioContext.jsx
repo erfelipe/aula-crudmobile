c7f2dd1791e94d2da48c76988da9f30665e8dc41
Felipe <erfelipe@yahoo.com.br>
2023-11-12 12:15:07 -0300

    first commit

---
+++ src/contexts/UsuarioContext.jsx
@@ -0,0 +1,88 @@
+import React, { createContext, useState } from 'react'
+import axios from 'axios';
+export const UsuarioContext = createContext({});
+
+export default function UsuarioProvider({ children }) {
+  const [id, setId] = useState("");
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [altura, setAltura] = useState("");
+  const [peso, setPeso] = useState("");
+  const [usuarios, setUsuarios] = useState([]);
+  const [atualizacao, setAtualizacao] = useState({});
+
+  const url = "https://pessoas-git-one.vercel.app/usuarios/";
+
+  function buscarUsuarios() {
+    fetch(url)
+      .then((respFetch) => respFetch.json())
+      .then((respJson) => setUsuarios(respJson))
+      .catch((erro) => console.warn(erro))
+  }
+
+  function gravarDados() {
+    console.log("gravar dados", url + id)
+    if (id) {
+      axios.put(url + id, {
+        nome: nome,
+        email: email,
+        altura: altura,
+        peso: peso,
+      }).then((resp) => atualizaListaUsuarioEditado(resp)).catch((erro) => console.log(erro));
+    } else {
+      axios.post(url, {
+        nome: nome,
+        email: email,
+        altura: (altura ? altura : null),
+        peso: (peso ? peso : null),
+      }).then((resp) => atualizaListaUsuarioNovo(resp)).catch((erro) => console.log(erro));
+    }
+  }
+
+  function atualizaListaUsuarioEditado(response) {
+    console.log(response);
+    console.log(response.data);
+    console.log(response.data, "edit");
+
+    let { id } = response.data;
+    const index = usuarios.findIndex(item => item.id == id);
+    let users = usuarios;
+    users[index].nome = nome;
+    users[index].email = email;
+    users[index].altura = altura;
+    users[index].peso = peso;
+    setUsuarios(users);
+
+    let usuario = {};
+    usuario.id = id;
+    usuario.nome = nome;
+    usuario.email = email;
+    usuario.altura = altura;
+    usuario.peso = peso;
+    console.log(usuario);
+    setAtualizacao(usuario);
+  }
+
+  function atualizaListaUsuarioNovo(response) {
+    console.log(response);
+    let { id, nome, email, altura, peso } = response.data;
+    let obj = { "id": id, "nome": nome, "email": email, "altura": altura, "peso": peso };
+    let users = usuarios;
+    users.push(obj);
+    setUsuarios(users);
+    setAtualizacao(obj);
+  }
+
+  function apagarUsuario(cod) {
+    axios.delete(url + cod)
+    .then(() => {
+      setUsuarios(usuarios.filter(item => item.id !== cod));})
+    .catch( (erro) => console.log(erro) )
+  }
+
+  return (
+    <UsuarioContext.Provider value={{ id, nome, email, altura, peso, setId, setNome, setEmail, setAltura, setPeso, buscarUsuarios, gravarDados, usuarios, apagarUsuario, atualizacao }} >
+      {children}
+    </UsuarioContext.Provider>
+  )
+}
